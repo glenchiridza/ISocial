18af5d96a984d0117370f9234fec20aa143e8fdb
glenchiridza <glenchiridz@gmail.com>
2022-05-19 15:53:58 +0200

    presenter setup

--- app/src/main/java/com/glencconnnect/isocial/SetupActivity.java
+++ app/src/main/java/com/glencconnnect/isocial/SetupActivity.java
@@ -3,12 +3,23 @@ package com.glencconnnect.isocial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SetupActivity extends AppCompatActivity {
+
+    private EditText edtUsername, edtFullName, edtCountry;
+    private Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+
+        edtUsername = findViewById(R.id.st_username);
+        edtFullName = findViewById(R.id.st_fullname);
+        edtCountry = findViewById(R.id.st_country);
+        btnSave = findViewById(R.id.btn_save);
+
     }
 }
